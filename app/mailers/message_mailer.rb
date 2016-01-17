cc8f30ce35e8284510ba2f1d177cef2151fd088b
Katherine Abu Hadal <katherine.abu.hadal@gmail.com>
2016-01-17 14:01:05 -0500

    Change from address for contact form

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,7 +3,7 @@ class MessageMailer < ApplicationMailer
   def new_message_email(message)
     @message = message
 
-    mail subject: "Message from #{message.name}"
+    mail from: @message.email, subject: "Message from #{message.name}"
   end
 
 end
