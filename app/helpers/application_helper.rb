4e4e62189c6a95f8cab206eb3e3f9eeab356e43f
Katherine Abu Hadal <katherine.abu.hadal@gmail.com>
2016-01-19 06:49:49 -0500

    Set metacharset to utf 8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     options[:media] ||= ''
     options[:description] ||= 'Sheba Yemeni Food'
     url = "//www.pinterest.com/pin/create/button/?"
-    url << options.collect { |key, value| "#{key}=#{value}" }.join('&')
+    url << options.collect { |key, value| "#{key}=#{value.to_s.force_encoding("UTF-8")}" }.join('&')
     link_to(image_tag("//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png", { alt: 'Pin it'}), url, { 'data-pin-do' => "buttonPin", 'data-pin-config' => "beside" })
   end
 
