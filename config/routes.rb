e30d9245b2ea48323b33d619dc32d292ef3c9db5
Katherine Abu Hadal <katherine.abu.hadal@gmail.com>
2016-03-13 14:10:59 -0400

    Change about to name

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
   get "/content/fatah", to: redirect('/content/susi-fattah', status: 301)
   get "/spices/zurbiyan", to: redirect('/', status: 301)
   get "/blog", to: redirect('/', status: 301)
+  get "/about", to: redirect('/katherineabuhadal', status: 301)
   get "/lunch/saltah", to: redirect('/content/saltah', status: 301)
   get "/spices/%D8%A8%D9%86%D8%AA-%D8%A7%D9%84%D8%B5%D8%AD%D9%86", to: redirect('/tags/%D8%A8%D9%86%D8%AA-%D8%A7%D9%84%D8%B5%D8%AD%D9%86', status: 301)
 
